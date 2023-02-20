b63959973b6132e0898d317d57207c37653816fd
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-02-20 16:29:39 +0530

    add config files

---
+++ beverly-hills/start-nethermind.sh
@@ -0,0 +1,3 @@
+cd src/Nethermind/Nethermind.Runner
+dotnet run -c release --config=BeverlyHills --Merge.TerminalTotalDifficulty=0 --Pruning.Mode=None --JsonRpc.EnginePort=8551 \
+--Network.DiscoveryPort=30303 --JsonRpc.JwtSecretFile=/tmp/jwtsecret --Init.AutoDump Parity
