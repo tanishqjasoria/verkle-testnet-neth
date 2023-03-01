0ed5bc0018eef662a373016f17a397e1b9a3511f
Hubert Ritzdorf <10403309+ritzdorf@users.noreply.github.com>
2023-03-01 19:38:39 +0100

    Fix Path and Enable Fail-on-Error

--- beverly-hills/setup.sh
+++ beverly-hills/setup.sh
@@ -1,9 +1,11 @@
+set -e
+
 openssl rand -hex 32 | tr -d "\n" > /tmp/jwtsecret
 
 mkdir teku
 cd teku
 wget https://artifacts.consensys.net/public/teku/raw/names/teku.tar.gz/versions/23.1.1/teku-23.1.1.tar.gz
-tar -xvf teku-23.1.1
+tar -xvf teku-23.1.1.tar.gz
 
 cd ..
 git clone https://github.com/nethermindeth/nethermind --recursive
