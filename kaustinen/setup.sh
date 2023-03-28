4f4cff40530724012581b9866b537e499fd6e48f
Tanishq Jasoria <jasoriatanishq@gmail.com>
2023-03-28 11:33:50 +0530

    use lighthouse binary instead and update kaustinen

--- kaustinen/setup.sh
+++ kaustinen/setup.sh
@@ -4,14 +4,8 @@ openssl rand -hex 32 | tr -d "\n" > /tmp/jwtsecret
 
 sudo apt install -y git gcc g++ make cmake pkg-config llvm-dev libclang-dev clang protobuf-compiler
 
-git clone https://github.com/sigp/lighthouse.git lighthouse-git
-cd lighthouse-git
-git remote add macladson https://github.com/macladson/lighthouse.git
-git fetch macladson
-git checkout verkle-trees-full
-make
-cp target/release/lighthouse ../.
-cd ..
+wget https://github.com/tanishqjasoria/lighthouse/releases/download/verkle-full/lighthouse-verkle-full-x86_64-unknown-linux-gnu.tar.gz
+tar -xvf lighthouse-verkle-full-x86_64-unknown-linux-gnu.tar.gz
 
 # mkdir teku
 # cd teku
